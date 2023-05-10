72a77fc2b61a67ab3774f9e9910d97abdd5d633c
Florin STAN <597933+namtzigla@users.noreply.github.com>
2023-05-10 11:04:36 -0600

    Delete doc.go

--- doc.go
+++
@@ -1,6 +0,0 @@
-package api
-
-//go:generate go install github.com/bufbuild/buf/cmd/buf@latest
-//go:generate go install git.tcncloud.net/tools/protoc-gen-sq@latest
-//go:generate env PATH=$PATH:$GOPATH/bin $GOPATH/bin/buf mod update
-//go:generate env PATH=$PATH:$GOPATH/bin $GOPATH/bin/buf generate
