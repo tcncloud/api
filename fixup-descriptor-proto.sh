84b80f8c75021233a60a6fbb134c2b0fa2fd96ab
Alexandru Pirvulescu <alex@tcnbroadcasting.com>
2023-06-06 17:47:42 +0300

    CWD is .

--- fixup-descriptor-proto.sh
+++ fixup-descriptor-proto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo removing descriptor.proto from everywhere
-find ${REPO_NAME} -name "*.proto" -exec sed -i '/descriptor.proto/ d' {} \; 
+find . -name "*.proto" -exec sed -i '/descriptor.proto/ d' {} \; 
 
 echo adding descriptor.proto to everywhere
-find ${REPO_NAME} -name "*.proto" -exec sed -i '/^package/ import "google/protobuf/descriptor.proto";' {} \;
+find . -name "*.proto" -exec sed -i '/^package/ import "google/protobuf/descriptor.proto";' {} \;
