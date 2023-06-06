59018c286eb89d04833d4ff49ab7b984302da3e7
Alexandru Pirvulescu <alex@tcnbroadcasting.com>
2023-06-06 17:50:38 +0300

    fix the fixup script

--- fixup-descriptor-proto.sh
+++ fixup-descriptor-proto.sh
@@ -4,4 +4,4 @@ echo removing descriptor.proto from everywhere
 find . -name "*.proto" -exec sed -i '/descriptor.proto/ d' {} \; 
 
 echo adding descriptor.proto to everywhere
-find . -name "*.proto" -exec sed -i '/^package/ import "google/protobuf/descriptor.proto";' {} \;
+find . -name "*.proto" -exec sed -i '/^package/ a import "google/protobuf/descriptor.proto";' {} \;
